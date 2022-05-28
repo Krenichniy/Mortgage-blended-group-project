d0992b22c3ce2e2015bdef80c03caa6107db5a8b
AnastasiiaLAV <lav21051991@gmail.com>
2022-05-28 16:16:46 +0300

    add style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const refs = {
-  formLinks: null,
-  divRoot: document.getElementById("root"),
-  buttonCreateFormRefs: null,
+    formLinks: null,
+    divRoot: document.getElementById("root"),
+    buttonCreateFormRefs: null,
 };
 
 const BANK_LIST = [];
@@ -9,43 +9,52 @@ const BANK_LIST = [];
 function renderBanks() {}
 
 function createForm() {
-  const formCap = `<form id="createBank" class="form">
+    const formCap = `<form id="createBank" class="cardBank form">
         <label>
             Назва банку
-            <input name="name"/>
+            <input name="name" class="form-imput"/>
         </label>
         <label>
             Річна відсоткова ставка по іпотеці
-            <input name="interestRate" type="counter"/>
+            <input name="interestRate" type="counter"  class="form-imput"/>
         </label>
         <label>
             Максимальна сума кредиту, яку надає банк
-            <input name="maxLoan" type="counter"/>
+            <input name="maxLoan" type="counter" class="form-imput"/>
         </label>
         <label>
             Мінімальний платіж
-            <input name="minPayment" type="counter"/>
+            <input name="minPayment" type="counter" class="form-imput"/>
         </label>
         <label>
             Термін кредиту
-            <input name="loanTerm" type="counter"/>
+            <input name="loanTerm" type="counter" class="form-imput"/>
         </label>
-        <button type="submit" class="btn-createBank">submit</button>
+        <button type="submit" class="btn-createBank">
+         <svg class="icon icon-ok" width="20"; height="20";>
+         <use href="./icomoon/icons.svg#icon-ok"></use>
+         </svg>
+         <span>Додати</span>
+         </button>
     </form>`;
 
-  refs.divRoot.insertAdjacentHTML("afterbegin", formCap);
-  refs.formLinks = document.querySelector("#createBank");
-  refs.formLinks.addEventListener("submit", onSubmitForm);
+    refs.divRoot.insertAdjacentHTML("afterbegin", formCap);
+    refs.formLinks = document.querySelector("#createBank");
+    refs.formLinks.addEventListener("submit", onSubmitForm);
 }
+
 function init() {}
 
 function createFirstMarkup() {
-  const createBankMarkup = `<h2 class = "title_markup">Добав свій перший банк</h2>
-	<button type="button" class="btn-createBank">Додай банк</button
+    const createBankMarkup = `<h2 class = "title_markup">Добав свій перший банк</h2>
+	<button type="button" class="btn-createBank">
+    <svg class="icon icon-plus" width="20"; height="20"><use href="./icomoon/icons.svg#icon-plus"></use></svg>
+    <span>Додай банк</span>
+    </button
   >`;
 
-  refs.divRoot.insertAdjacentHTML("afterbegin", createBankMarkup);
-  refs.buttonCreateFormRefs = document.querySelector(".btn-createBank");
+    refs.divRoot.insertAdjacentHTML("afterbegin", createBankMarkup);
+    refs.buttonCreateFormRefs = document.querySelector(".btn-createBank");
 }
 
 createFirstMarkup();
@@ -53,13 +62,14 @@ createFirstMarkup();
 refs.buttonCreateFormRefs.addEventListener("click", openForm);
 
 function openForm() {
-  refs.divRoot.innerHTML = "";
-  createForm();
+    refs.divRoot.innerHTML = "";
+    createForm();
 }
 document.querySelector(".btn-createBank");
+
 function createBank(e) {
-  const obj = {};
-  console.log(e.currentTarget.value);
+    const obj = {};
+    console.log(e.currentTarget.value);
 }
 
 // {
@@ -72,24 +82,24 @@ function createBank(e) {
 // },
 
 const onSubmitForm = (e) => {
-  e.preventDefault();
-  const isValid = [...e.target.elements].filter(elem => elem.nodeName === "INPUT").every(elem => 
-    elem.value !== ""
-  )
-  if (isValid) {
-    const { name, interestRate, maxLoan, minPayment, loanTerm } = e.target.elements;
+    e.preventDefault();
+    const isValid = [...e.target.elements].filter(elem => elem.nodeName === "INPUT").every(elem =>
+        elem.value !== ""
+    )
+    if (isValid) {
+        const { name, interestRate, maxLoan, minPayment, loanTerm } = e.target.elements;
 
-  const newBank = {
-    id: new Date(),
-    name: name.value,
-    interestRate: interestRate.value,
-    maxLoan: maxLoan.value,
-    minPayment: minPayment.value,
-    loanTerm: loanTerm.value,
-  };
-    BANK_LIST.push(newBank);
-  }
-  
-  
-  
+        const newBank = {
+            id: new Date(),
+            name: name.value,
+            interestRate: interestRate.value,
+            maxLoan: maxLoan.value,
+            minPayment: minPayment.value,
+            loanTerm: loanTerm.value,
+        };
+        BANK_LIST.push(newBank);
+    }
+
+
+
 };
